655368d9275ae91d159c7c22020d92f54735d8f3
yi zheng <847399102@qq.com>
2021-11-23 10:06:01 +0800

    finish login&register in WEB

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -443,14 +443,14 @@ class AcGamePlayground {
         this.$playground.hide();
     }
 }
-class Settings{
-    constructor(root){
+class Settings {
+    constructor(root) {
         this.root = root;
         this.platform = "WEB";
-        if(this.root.AcWingOS) this.platform = "ACAPP";
+        if (this.root.AcWingOS) this.platform = "ACAPP";
         this.username = "";
         this.photo = "";
-        
+
         this.$settings = $(`
 <div class="ac-game-settings">
     <div class="ac-game-settings-login">
@@ -472,14 +472,14 @@ class Settings{
                 <button>登录</button>
             </div>
         </div>
-        <div class="ac-game-settings-error-messages">
+        <div class="ac-game-settings-error-message">
         </div>
         <div class="ac-game-settings-option">
             注册
         </div>
         <br>
         <div class="ac-game-settings-acwing">
-            <img width="25" src="http://119.91.23.137:8000/static/image/settings/acwing_logo.png">
+            <img width="30" src="http://119.91.23.137:8000/static/image/settings/acwing_logo.png">
             <br>
             <div>
                 QQ一键登录
@@ -504,33 +504,44 @@ class Settings{
             <div class="ac-game-settings-item">
                 <input type="password" placeholder="确认密码">
             </div>
-         </div>
-         <div class="ac-game-settings-submit">
+        </div>
+        <div class="ac-game-settings-submit">
             <div class="ac-game-settings-item">
                 <button>注册</button>
             </div>
-         </div>
-         <div class="ac-game-settings-error-message">
-         </div>
-         <div class="ac-game-settings-option">
+        </div>
+        <div class="ac-game-settings-error-message">
+        </div>
+        <div class="ac-game-settings-option">
             登录
-         </div>
-         <br>
-         <div class="ac-game-settings-acwing">
-            <img width="25" src="http://119.91.23.137:8000/static/image/settings/acwing_logo.png">
+        </div>
+        <br>
+        <div class="ac-game-settings-acwing">
+            <img width="30" src="http://119.91.23.137:8000/static/image/settings/acwing_logo.png">
             <br>
             <div>
                 QQ一键登录
             </div>
-         </div>
+        </div>
     </div>
 </div>
 `);
         this.$login = this.$settings.find(".ac-game-settings-login");
-        
+        this.$login_username = this.$login.find(".ac-game-settings-username input");
+        this.$login_password = this.$login.find(".ac-game-settings-password input");
+        this.$login_submit = this.$login.find(".ac-game-settings-submit button");
+        this.$login_error_message = this.$login.find(".ac-game-settings-error-message");
+        this.$login_register = this.$login.find(".ac-game-settings-option");
+
         this.$login.hide();
 
         this.$register = this.$settings.find(".ac-game-settings-register");
+        this.$register_username = this.$register.find(".ac-game-settings-username input");
+        this.$register_password = this.$register.find(".ac-game-settings-password-first input");
+        this.$register_password_confirm = this.$register.find(".ac-game-settings-password-second input");
+        this.$register_submit = this.$register.find(".ac-game-settings-submit button");
+        this.$register_error_message = this.$register.find(".ac-game-settings-error-message");
+        this.$register_login = this.$register.find(".ac-game-settings-option");
 
         this.$register.hide();
 
@@ -539,54 +550,144 @@ class Settings{
         this.start();
     }
 
-    start(){
+    start() {
         this.getinfo();
+        this.add_listening_events();
     }
 
-    register(){  //打开注册界面
+    add_listening_events() {
+        this.add_listening_events_login();
+        this.add_listening_events_register();
+    }
+
+    add_listening_events_login() {
+        let outer = this;
+
+        this.$login_register.click(function() {
+            outer.register();
+        });
+        this.$login_submit.click(function() {
+            outer.login_on_remote();
+        });
+    }
+
+    add_listening_events_register() {
+        let outer = this;
+        this.$register_login.click(function() {
+            outer.login();
+        });
+        this.$register_submit.click(function() {
+            outer.register_on_remote();
+        });
+    }
+
+    login_on_remote() {  // 在远程服务器上登录
+        let outer = this;
+        let username = this.$login_username.val();
+        let password = this.$login_password.val();
+        this.$login_error_message.empty();
+
+        $.ajax({
+            url: "http://119.91.23.137:8000/settings/login/",
+            type: "GET",
+            data: {
+                username: username,
+                password: password,
+            },
+            success: function(resp) {
+                console.log(resp);
+                if (resp.result === "success") {
+                    location.reload();
+                } else {
+                    outer.$login_error_message.html(resp.result);
+                }
+            }
+        });
+    }
+
+    register_on_remote() {  // 在远程服务器上注册
+        let outer = this;
+        let username = this.$register_username.val();
+        let password = this.$register_password.val();
+        let password_confirm = this.$register_password_confirm.val();
+        this.$register_error_message.empty();
+
+        $.ajax({
+            url: "http://119.91.23.137:8000/settings/register/",
+            type: "GET",
+            data: {
+                username: username,
+                password: password,
+                password_confirm: password_confirm,
+            },
+            success: function(resp) {
+                console.log(resp);
+                if (resp.result === "success") {
+                    location.reload();  // 刷新页面
+                } else {
+                    outer.$register_error_message.html(resp.result);
+                }
+            }
+        });
+    }
+
+    logout_on_remote() {  // 在远程服务器上登出
+        if (this.platform === "ACAPP") return false;
+
+        $.ajax({
+            url: "http://119.91.23.137:8000/settings/logout/",
+            type: "GET",
+            success: function(resp) {
+                console.log(resp);
+                if (resp.result === "success") {
+                    location.reload();
+                }
+            }
+        });
+    }
+
+    register() {  // 打开注册界面
         this.$login.hide();
         this.$register.show();
     }
 
-    login(){    //打开登陆界面
+    login() {  // 打开登录界面
         this.$register.hide();
         this.$login.show();
     }
 
-
-    getinfo(){
+    getinfo() {
         let outer = this;
 
         $.ajax({
             url: "http://119.91.23.137:8000/settings/getinfo/",
             type: "GET",
-            data:{
+            data: {
                 platform: outer.platform,
             },
             success: function(resp) {
                 console.log(resp);
-                if(resp.result === "success"){
+                if (resp.result === "success") {
                     outer.username = resp.username;
                     outer.photo = resp.photo;
                     outer.hide();
                     outer.root.menu.show();
-                }else{
-                    outer.register();
+                } else {
+                    outer.login();
                 }
             }
         });
     }
-    
-    hide(){
+
+    hide() {
         this.$settings.hide();
     }
-    
-    show(){
+
+    show() {
         this.$settings.show();
     }
-
-
 }
+
 export class AcGame {
     constructor(id, AcWingOS) {
         this.id = id;
